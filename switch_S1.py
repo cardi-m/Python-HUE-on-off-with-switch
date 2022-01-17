a57f243018a2b82afb570c72c64d79624b3555b9
cardi <84557922+cardi-m@users.noreply.github.com>
2022-01-17 17:26:14 +0100

    Update switch_S1.py
    Hier schonmal der Versuch etwas mehr zu kommentieren.

--- switch_S1.py
+++ switch_S1.py
@@ -6,45 +6,53 @@ import datetime
 
 logging.basicConfig()
 
+#Die GPIO sind meinen eingänge am Raspi Mit GPIO7 wir Schalter1 = S1 ausgewertet
 GPIO.setmode(GPIO.BOARD)
 GPIO_S1 = 7
 GPIO.setup(GPIO_S1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+#Setzten der Variablen auf 0, um einen definierten Anfangszustand zu haben
+Current_State_S1 = 0
+Previous_State_S1 = 0
 
-Current_State = 0
-Previous_State = 0
-
+#definieren der HUE-Bridge
 b = Bridge('192.168.xxx.xxx')
 
-# If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single$
+#Bekanntmachen des scriptes in der Bridge
+#If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single$
 #b.connect()
 
+#Prüfung dass der Schalter ausgeschaltet ist
 try:
  print "Schalter ist EINgeschaltet, bitte AUSschalten"
  while GPIO.input(GPIO_S1)==1:
-  Current_State = 0 
+  Current_State_S1 = 0 
  print "bereit"
 
 #Schleife bis Strg+C
  while True :
-  Current_State = GPIO.input(GPIO_S1)
+  Current_State_S1 = GPIO.input(GPIO_S1)
 
-  if Current_State==1 and Previous_State==0:
+  if Current_State_S1==1 and Previous_State_S1==0:
    #GPIO.wait_for_edge (7, GPIO.RISING)
    b.set_light(1, 'on', True)
+   b.set_light(9, 'on', True)
    print "%s: eingeschaltet" % datetime.datetime.now()
-   Previous_State=1
+   Previous_State_S1=1
    time.sleep (1)
 
-  elif  Current_State==0 and Previous_State==1:
+  elif  Current_State_S1==0 and Previous_State_S1==1:
    #GPIO.wait_for_edge (7, GPIO.FALLING)
    b.set_light(1, 'on', False)
+   b.set_light(9, 'on', False)
    print "%s: ausgeschaltet" % datetime.datetime.now()
-   Previous_State=0
+   Previous_State_S1=0
    time.sleep  (1)
 
   time.sleep (0.01)
 
+#Change the light state
+#b.set_light(1, 'on', True)
 
 except KeyboardInterrupt:
  print "Beenden"
